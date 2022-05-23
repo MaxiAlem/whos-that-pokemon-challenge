839dfcbeb639f117378cb65a3103ef05e3684b30
MaxiAlem <maerc.maxi@gmail.com>
2022-05-23 01:36:51 -0300

    bug n el contador a arreglar

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {Pokemon} from './types'
 function App() {
   const {random} = api
   
-  const[count, setCount] = useState(0)
+  let[count, setCount] = useState(0)
   const [pkmn, setPkmon] = useState('')
   const [pkmnName, setPkmonName]=useState<string>('name')
   const [pkmnImg, setPkmonImg]=useState('img')
@@ -29,53 +29,8 @@ function App() {
  
 async function pokemon(res:object) {
   clearHtml()
-  // const {name, id, image}:{name:string, id:number, image:string} = res
-  const pokmn : Pokemon = res
-  console.log(pokmn.name)
-  console.log(pokmn.id)
-  console.log(pokmn.image)
-  
-  //gen html
-  const box = document.createElement('div');
-
-  const pokeGuess = document.createElement('form')
-
-  const textGuess = document.createElement('input')
-  textGuess.setAttribute('type', 'text');
-  textGuess.setAttribute('autocomplete', 'off')
-
-  const submitGuess = document.createElement('input')
-  submitGuess.setAttribute('type', 'submit');
-  submitGuess.value = 'adivinar'
-  submitGuess.onclick =function(e){
-    e.preventDefault()
-    if(textGuess.value === pokmn.name){
-      console.log('bien')
-      setCount(count + 1)
-    }else{
-      console.log('mal')
-    }
-    
-  }
-
-  pokeGuess.appendChild(textGuess);
-  pokeGuess.appendChild(submitGuess);
-
-
-  const pokeId = document.createElement('h2')
-  pokeId.innerHTML = `${pokmn.id}`;
-
-  const pokeImg = document.createElement('img')
-  pokeImg.className = 'pokeImg'
-  pokeImg.src = `${pokmn.image}`;
-
-
-  box.appendChild(pokeImg);
-  box.appendChild(pokeGuess);
-  box.appendChild(pokeId);
-  
-
-  newpkmn.appendChild(box)
+ 
+   genHTML(res)
 
 }
 
@@ -84,17 +39,79 @@ function clearHtml(){
     newpkmn.removeChild(newpkmn.firstChild)
   }
 }
+
+function genHTML(res:object){
+   
+   // const {name, id, image}:{name:string, id:number, image:string} = res
+   const pokmn : Pokemon = res
+   console.log(pokmn.name)
+   console.log(pokmn.id)
+   console.log(pokmn.image)
+  //gen html
+
+   const box = document.createElement('div');
+
+   const pokeGuess = document.createElement('form')
+ 
+   const textGuess = document.createElement('input')
+   textGuess.setAttribute('type', 'text');
+   textGuess.setAttribute('autocomplete', 'off')
+ 
+   const submitGuess = document.createElement('input')
+   submitGuess.setAttribute('type', 'submit');
+   submitGuess.value = 'adivinar'
+   submitGuess.onclick =async function(e){
+     e.preventDefault()
+     if(textGuess.value === pokmn.name){
+       console.log('bien')
+        clearHtml()
+        getData()
+        setCount(count +1) 
+     }else{
+       console.log('mal')
+       clearHtml()
+       await getData()
+        
+
+       setCount(count = 0)
+     }
+     
+   }
+ 
+   pokeGuess.appendChild(textGuess);
+   pokeGuess.appendChild(submitGuess);
+ 
+ 
+  //  const pokeId = document.createElement('h2')
+  //  pokeId.innerHTML = `${pokmn.id}`;
+ 
+   const pokeImg = document.createElement('img')
+   pokeImg.className = 'pokeImg'
+   pokeImg.src = `${pokmn.image}`;
+ 
+ 
+   box.appendChild(pokeImg);
+   box.appendChild(pokeGuess);
+  // box.appendChild(pokeId);
+   
+ 
+   newpkmn.appendChild(box)
+ 
+}
   return (
   <Fragment>
       
       <main>
-      <p>You clicked {count} times</p>
-     
-      <p>pokemon</p>
+      
      
       <button onClick={()=>{getData()}}>new Pokemon</button>
       
        <div id="newpkmn"></div>
+       
+       
+       <p>You have {count} pokepoints</p>
+       <button onClick={()=>{setCount(count +1)}}>new score</button>
+       <button onClick={()=>{setCount(count +1)}}>new score</button>
       </main>
 
   </Fragment>)
